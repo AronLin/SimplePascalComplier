1b5ab859f8f3333efc4b6a5c454fb2e70359959a
Guangchen Lin <347630870@qq.com>
2018-06-10 04:08:19 +0800

    merge冲突解决，加入errorhandle后，yyinput被重写，从file中提取

--- main.cpp
+++ main.cpp
@@ -5,15 +5,17 @@
 #include "errorhandle.h"
 
 extern int yyparse();
-extern FILE* yyin;
 extern AbstractTree::ProgramNode* astRoot;
+int ParseError=0;
+FILE* file;
 
 int main()
 {
-    FILE* fh;
-    fh = fopen("/home/qian/SharedFolders/Compiler/Code/SimplePascalComplier/pas/1.pas", "r");
-    yyin = fh;
+    file = fopen("/home/qian/SharedFolders/Compiler/Code/SimplePascalComplier/pas/1.pas", "r");
+    init_error_handle();
     yyparse();
+    close_error_handle();
+    if (ParseError) return 0;
     CodeGenContext context;
     astRoot->CodeGen(context);
     context.Builder.CreateRetVoid();
